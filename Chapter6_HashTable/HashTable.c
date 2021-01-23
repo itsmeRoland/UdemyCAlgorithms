bd5245af89886174a14807910731f6b0677c2f2c
Jan S <schaffranek.jan@web.de>
2021-01-23 14:37:17 +0100

    up

--- Chapter6_HashTable/HashTable.c
+++ Chapter6_HashTable/HashTable.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "HashTable.h"
 
@@ -13,10 +12,19 @@ hash_table_t *createHashTable(void)
         return NULL;
     }
 
-    for (uint32_t i = 0u; i < TABLE_SIZE; i++)
+    size_t data_size = TABLE_SIZE * sizeof(item_t);
+    item_t *data = (item_t *)malloc(data_size);
+
+    if (NULL == data)
     {
-        hash_table->table[i].value = NO_VALUE;
+        free(hash_table);
+
+        return NULL;
     }
+
+    hash_table->size = 0u;
+    hash_table->capacity = TABLE_SIZE;
+    hash_table->data = data;
 
     return hash_table;
 }
@@ -25,79 +33,34 @@ hash_table_t *freeHashTable(hash_table_t *hash_table)
 {
     if (NULL != hash_table)
     {
+        if (NULL != hash_table->data)
+        {
+            free(hash_table->data);
+        }
+
         free(hash_table);
     }
 
     return NULL;
 }
 
-uint32_t hash(const char key[MAX_NAME_SIZE])
+uint32_t hash(char key[MAX_NAME_SIZE])
 {
-    size_t key_length = strlen(key);
     uint32_t hash_value = 0u;
 
-    for (uint32_t i = 0; i < key_length; i++)
+    for (uint32_t i = 0; i < MAX_NAME_SIZE; i++)
     {
-        hash_value += key[i];
-        hash_value = (hash_value * key[i]) % TABLE_SIZE;
+        if ('\0' != key[i])
+        {
+            hash_value = (hash_value * 128 + key[i]) % TABLE_SIZE;
+        }
+        else
+        {
+            break;
+        }
     }
 
     return hash_value;
-}
-
-void insertItem(hash_table_t *hash_table, item_t *item)
-{
-    for (uint32_t i = 0; i < TABLE_SIZE; i++)
-    {
-        int key_compare = strncmp(hash_table->table[i].key, item->key, MAX_NAME_SIZE);
-        if (0 == key_compare)
-        {
-            hash_table->table[i].value = item->value;
-
-            return;
-        }
-    }
-
-    uint32_t hash_value = hash(item->key);
-
-    while (NO_VALUE != hash_table->table[hash_value].value)
-    {
-        hash_value = (hash_value + 1u) % TABLE_SIZE;
-    }
-
-    hash_table->table[hash_value] = *item;
-}
-
-value_type_t removeItem(hash_table_t *hash_table, char key[MAX_NAME_SIZE])
-{
-    uint32_t hash_value = hash(key);
-    item_t *item = &hash_table->table[hash_value];
-
-    while (0 != strncmp(key, item->key, MAX_NAME_SIZE))
-    {
-        hash_value = (hash_value + 1u) % TABLE_SIZE;
-        item = &hash_table->table[hash_value];
-    }
-
-    value_type_t value = hash_table->table[hash_value].value;
-
-    hash_table->table[hash_value].value = NO_VALUE;
-    strcpy(hash_table->table[hash_value].key, DELETED_KEY);
-
-    return value;
-}
-
-value_type_t getValue(hash_table_t *hash_table, char key[MAX_NAME_SIZE])
-{
-    for (uint32_t i = 0; i < TABLE_SIZE; i++)
-    {
-        if (0 == strncmp(hash_table->table[i].key, key, MAX_NAME_SIZE))
-        {
-            return hash_table->table[i].value;
-        }
-    }
-
-    return NO_VALUE;
 }
 
 void printHashTable(hash_table_t *hash_table)
@@ -107,11 +70,15 @@ void printHashTable(hash_table_t *hash_table)
         return;
     }
 
-    printf("\nHashTable values:\n");
+    printf(
+        "\nHashTable contains %u elements with a capcity of %u.\n",
+        hash_table->size,
+        hash_table->capacity
+    );
 
-    for (uint32_t i = 0u; i < TABLE_SIZE; i++)
+    for (uint32_t i = 0u; i < hash_table->size; i++)
     {
-        item_t *item = &hash_table->table[i];
-        printf("Index: %d, Key: %s, Value: %f\n", i, item->key, item->value);
+        item_t *item = &hash_table->data[i];
+        printf("Index: %d, Key: %s, Value %f\n", i, item->key, item->value);
     }
 }
--- Chapter6_HashTable/HashTable.h
+++ Chapter6_HashTable/HashTable.h
@@ -1,21 +1,22 @@
-#ifndef HASH_TABLE_H
-#define HASH_TABLE_H
+#ifndef QUEUE_H
+#define QUEUE_H
 
-/*********************/
-/*      INCLUDES     */
-/*********************/
+/*******************/
+/*     INCLUDES    */
+/*******************/
 
 #include <math.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-/*********************/
-/* DEFINES AND TYPES */
-/*********************/
+/**********************/
+/* DEFINES AND TYPES  */
+/**********************/
 
 typedef float value_type_t;
 #define NO_VALUE (value_type_t)INFINITY
 
+#define TABLE_SIZE (uint32_t)13u
 #define MAX_NAME_SIZE (uint32_t)256u
 #define DELETED_KEY "DELETED"
 
@@ -25,29 +26,23 @@ typedef struct item
     value_type_t value;
 } item_t;
 
-#define TABLE_SIZE (uint32_t)5u
-
 typedef struct hash_table
 {
-    item_t table[TABLE_SIZE];
+    uint32_t size;
+    uint32_t capacity;
+    item_t *data;
 } hash_table_t;
 
-/*********************/
-/*    FUNCTIONS      */
-/*********************/
+/**********************/
+/*     FUNCTIONS      */
+/**********************/
 
 hash_table_t *createHashTable(void);
 
 hash_table_t *freeHashTable(hash_table_t *hash_table);
 
-uint32_t hash(const char key[MAX_NAME_SIZE]);
-
-void insertItem(hash_table_t *hash_table, item_t *item);
-
-value_type_t removeItem(hash_table_t *hash_table, char key[MAX_NAME_SIZE]);
-
-value_type_t getValue(hash_table_t *hash_table, char key[MAX_NAME_SIZE]);
+uint32_t hash(char key[MAX_NAME_SIZE]);
 
 void printHashTable(hash_table_t *hash_table);
 
-#endif // HASH_TABLE_H
+#endif // QUEUE_H
