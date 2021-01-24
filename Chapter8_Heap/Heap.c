11976fa1e0b7adfd8d3f2fef48da6c403723e642
Jan S <schaffranek.jan@web.de>
2021-01-24 17:40:31 +0100

    up

--- Chapter8_Heap/Heap.c
+++ Chapter8_Heap/Heap.c
@@ -4,7 +4,7 @@
 
 #include "Heap.h"
 
-heap_t *createHeap(value_type_t value)
+heap_t *createHeap()
 {
     heap_t *heap = (heap_t *)malloc(sizeof(heap_t));
 
@@ -45,10 +45,131 @@ heap_t *freeHeap(heap_t *heap)
     return NULL;
 }
 
+uint32_t parentNode(uint32_t idx)
+{
+    return (idx - 1u) / 2u;
+}
+
+uint32_t leftChildNode(uint32_t idx)
+{
+    return (idx * 2u) + 1u;
+}
+
+uint32_t rightChildNode(uint32_t idx)
+{
+    return (idx * 2u) + 2u;
+}
+
+void swap(value_type_t *a, value_type_t *b)
+{
+    value_type_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertValue(heap_t *heap, value_type_t value)
+{
+    if (NULL == heap)
+    {
+        return;
+    }
+
+    if (heap->size == heap->capacity)
+    {
+        return;
+    }
+
+    heap->size++;
+    uint32_t idx = heap->size - 1u;
+    heap->data[idx] = value;
+
+    while (0u != idx && heap->data[parentNode(idx)] > heap->data[idx])
+    {
+        swap(&heap->data[idx], &heap->data[parentNode(idx)]);
+        idx = parentNode(idx);
+    }
+}
+
+value_type_t removeMinimum(heap_t *heap)
+{
+    if (NULL == heap)
+    {
+        return NO_VALUE;
+    }
+
+    if(heap->size == 1u)
+    {
+        heap->size--;
+
+        return heap->data[0];
+    }
+
+    uint32_t root = heap->data[0];
+    heap->data[0] = heap->data[heap->size - 1];
+    heap->size--;
+    heapify(heap, 0);
+
+    return root;
+}
+
+void heapify(heap_t *heap, uint32_t idx)
+{
+    if (NULL == heap)
+    {
+        return;
+    }
+
+    uint32_t left_idx = leftChildNode(idx);
+    uint32_t right_idx = rightChildNode(idx);
+    uint32_t smallest = idx;
+
+    if (left_idx < heap->size && heap->data[left_idx] < heap->data[smallest])
+    {
+        smallest =left_idx;
+    }
+
+    if (right_idx < heap->size && heap->data[right_idx] < heap->data[smallest])
+    {
+        smallest = right_idx;
+    }
+
+    if (smallest != idx)
+    {
+        swap(&heap->data[idx], &heap->data[smallest]);
+        heapify(heap, smallest);
+    }
+}
+
 void printHeap(heap_t *heap)
 {
     if (NULL == heap)
     {
         return;
+    }
+
+    uint32_t num_visited_nodes = 0u;
+    uint32_t depth = (uint32_t)ceil(log2(heap->size)) + 1u;
+
+    for (uint32_t i = 0u; i < depth; i++)
+    {
+        uint32_t num_layer_nodes = (uint32_t)pow(2.0, i);
+
+        printf("Layer: %u\n", i);
+
+        for (uint32_t j = 0u; j < num_layer_nodes; j++)
+        {
+            uint32_t current_idx = num_visited_nodes + j;
+
+            if (current_idx < heap->size)
+            {
+                printf("Node: %u, Value: %f\n", j, heap->data[current_idx]);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        num_visited_nodes += num_layer_nodes;
     }
 }
--- Chapter8_Heap/Main.c
+++ Chapter8_Heap/Main.c
@@ -5,7 +5,30 @@
 
 int main(void)
 {
-    heap_t *heap = createHeap(42.0f);
+    heap_t *heap = createHeap();
+
+    insertValue(heap, 42.0f);
+    insertValue(heap, 22.0f);
+    insertValue(heap, 60.0f);
+    insertValue(heap, 72.0f);
+    insertValue(heap, -20.0f);
+
+    assert(5u == heap->size);
+    assert(10u == heap->capacity);
+    assert(-20.0f == heap->data[0]);
+    assert(22.0f == heap->data[1]);
+    assert(60.0f == heap->data[2]);
+    assert(72.0f == heap->data[3]);
+    assert(42.0f == heap->data[4]);
+
+    printf("Start printing:\n");
+    printHeap(heap);
+
+    removeMinimum(heap);
+
+    printf("Start printing:\n");
+    printHeap(heap);
 
     heap = freeHeap(heap);
+    assert(NULL == heap);
 }
